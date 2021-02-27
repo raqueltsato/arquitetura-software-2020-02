a14b3fa5579e8c9cace61e6f1e8837a58fc7e536
Raquel <raquelsato1@gmail.com>
2021-02-27 14:08:41 -0300

    Calculo do valor ideal dos nutrientes com base na textura do solo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,13 +4,53 @@ public class App {
     private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Solo solo = new Solo();
+        Solo valorIdealSolo = new Solo();
         String textura;
+        double inputTemp;
 
         System.out.println("Escolha a textura de solo, digitando:");
         System.out.println("1 - para solo argiloso");
         System.out.println("2 - para solo textura média");
         textura = scan.nextLine();
         imprimeTextura(textura);
+        solo.setTextura(textura);
+
+        System.out.println("\nDigite o valor do fósforo");
+        solo.setFosforo(Double.parseDouble(scan.nextLine()));
+
+        System.out.println("\nDigite o valor do potássio");
+        solo.setPotassio(Double.parseDouble(scan.nextLine()));
+
+        System.out.println("\nDigite o valor do cálcio");
+        solo.setCalcio(Double.parseDouble(scan.nextLine()));
+
+        System.out.println("\nDigite o valor do magnésio");
+        solo.setMagnesio(Double.parseDouble(scan.nextLine()));
+
+        System.out.println("\nDigite o valor do enxofre");
+        solo.setEnxofre(Double.parseDouble(scan.nextLine()));
+
+        System.out.println("\nDigite o valor do alumínio");
+        solo.setAluminio(Double.parseDouble(scan.nextLine()));
+
+        System.out.println("\nDigite o valor do H+Al");
+        solo.setHal(Double.parseDouble(scan.nextLine()));
+
+        valorIdealSolo = solo.verificaValorIdeal();
+
+        System.out.println("\n------------ TABELA DE VALORES-------------");
+        System.out.println("\n          VALOR COLETADO          VALOR IDEAL");
+        System.out.println("Fosforo:     "+solo.getFosforo()+"                     "+ valorIdealSolo.getFosforo()+"\n");
+        System.out.println("Potássio:    "+solo.getPotassio()+"                     "+ valorIdealSolo.getPotassio()+"\n");
+        System.out.println("Cálcio:      "+solo.getCalcio()+"                     "+ valorIdealSolo.getCalcio()+"\n");
+        System.out.println("Magnésio:    "+solo.getMagnesio()+"                     "+ valorIdealSolo.getMagnesio()+"\n");
+        System.out.println("Enxofre:     "+solo.getEnxofre()+"                     "+ valorIdealSolo.getEnxofre()+"\n");
+        System.out.println("Alumínio:    "+solo.getAluminio()+"                     "+ valorIdealSolo.getAluminio()+"\n");
+        System.out.println("H+Al:        "+solo.getHal()+"                     "+ valorIdealSolo.getHal()+"\n");
+
+
+
 
 
 
@@ -18,12 +58,14 @@ public class App {
 
     private static void imprimeTextura(String textura) {
         if (textura.equals("1") ) {
-            System.out.printf("SOLO ARGILOSO");
+            System.out.printf("SOLO ARGILOSO\n");
         } else if(textura.equals("2") ) {
-            System.out.printf("SOLO TEXTURA MÉDIA");
+            System.out.printf("SOLO TEXTURA MÉDIA\n");
         } else {
-            System.out.printf("Você deve escolher 1 ou 2");
+            System.out.printf("Você deve escolher 1 ou 2\n");
             return;
         }
     }
+
+
 }
---
+++ src/main/java/Solo.java
@@ -0,0 +1,102 @@
+public class Solo {
+
+
+    String textura;
+    double fosforo;
+    double potassio;
+    double calcio;
+    double magnesio;
+    double enxofre;
+    double aluminio;
+    double hal;
+
+    public double getFosforo() {
+        return fosforo;
+    }
+
+    public void setFosforo(double fosforo) {
+        this.fosforo = fosforo;
+    }
+
+    public double getPotassio() {
+        return potassio;
+    }
+
+    public void setPotassio(double potassio) {
+        this.potassio = potassio;
+    }
+
+    public double getCalcio() {
+        return calcio;
+    }
+
+    public void setCalcio(double calcio) {
+        this.calcio = calcio;
+    }
+
+    public double getMagnesio() {
+        return magnesio;
+    }
+
+    public void setMagnesio(double magnesio) {
+        this.magnesio = magnesio;
+    }
+
+    public double getEnxofre() {
+        return enxofre;
+    }
+
+    public void setEnxofre(double enxofre) {
+        this.enxofre = enxofre;
+    }
+
+    public double getAluminio() {
+        return aluminio;
+    }
+
+    public void setAluminio(double aluminio) {
+        this.aluminio = aluminio;
+    }
+
+    public double getHal() {
+        return hal;
+    }
+
+    public void setHal(double hal) {
+        this.hal = hal;
+    }
+
+    public String getTextura() {
+        return textura;
+    }
+
+    public void setTextura(String textura) {
+        this.textura = textura;
+    }
+
+    public Solo verificaValorIdeal() {
+        Solo ideal = new Solo();
+        if (this.textura.equals("1")) {
+            ideal.setTextura("1");
+            ideal.setFosforo(9);
+            ideal.setPotassio(0.35);
+            ideal.setCalcio(6.0);
+            ideal.setMagnesio(1.5);
+            ideal.setEnxofre(9.0);
+            ideal.setAluminio(0);
+            ideal.setHal(0);
+        } else{
+            ideal.setTextura("2");
+            ideal.setFosforo(9);
+            ideal.setPotassio(0.25);
+            ideal.setCalcio(4.0);
+            ideal.setMagnesio(1.0);
+            ideal.setEnxofre(6.0);
+            ideal.setAluminio(0);
+
+        }
+        return ideal;
+    }
+
+
+}
