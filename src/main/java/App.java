41db798cdc48cac0cfb9de0724b2320f426defa4
Raquel <raquelsato1@gmail.com>
2021-03-05 20:30:44 -0300

    Memoria de calculo de correcao de fosforo e potassio

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -46,9 +46,18 @@ public class App {
         System.out.println("Cálcio:      "+solo.getCalcio()+"                     "+ valorIdealSolo.getCalcio()+"\n");
         System.out.println("Magnésio:    "+solo.getMagnesio()+"                     "+ valorIdealSolo.getMagnesio()+"\n");
         System.out.println("Enxofre:     "+solo.getEnxofre()+"                     "+ valorIdealSolo.getEnxofre()+"\n");
-        System.out.println("Alumínio:    "+solo.getAluminio()+"                     "+ valorIdealSolo.getAluminio()+"\n");
+        System.out.println("Alumínio:    "+solo.getAluminio()+"                      "+ valorIdealSolo.getAluminio()+"\n");
         System.out.println("H+Al:        "+solo.getHal()+"                     "+ valorIdealSolo.getHal()+"\n");
+        System.out.println("\n--------------------------------------------\n");
+        System.out.println("SCmol:       "+ scmolCalc(solo));
+        System.out.println("CTCCmol:     "+ ctcCmolCalc(solo));
+        System.out.println("V % Atual:   "+ vAtualCalc(solo));
 
+        System.out.println("\n--------------------------------------------\n");
+        System.out.println("\n-------------- APÓS CORREÇÃO---------------");
+        System.out.println("Fósforo após correção:        "+  fosforoAposCorrecao());
+        System.out.println("\n--------------------------------------------\n");
+        System.out.println("Potássio após correção:       "+  potassioAposCorrecao(solo));
 
 
 
@@ -57,6 +66,7 @@ public class App {
     }
 
     private static void imprimeTextura(String textura) {
+
         if (textura.equals("1") ) {
             System.out.printf("SOLO ARGILOSO\n");
         } else if(textura.equals("2") ) {
@@ -65,6 +75,44 @@ public class App {
             System.out.printf("Você deve escolher 1 ou 2\n");
             return;
         }
+    }
+
+    private static double scmolCalc(Solo solo) {
+        return solo.getPotassio()+solo.getCalcio()+solo.getMagnesio();
+    }
+
+    private static double ctcCmolCalc(Solo solo) {
+        return solo.getPotassio()+solo.getCalcio()+solo.getMagnesio()+solo.getHal();
+    }
+
+    private static double vAtualCalc(Solo solo) {
+        return 100 * scmolCalc(solo)/ctcCmolCalc(solo);
+    }
+
+    private static double fosforoAposCorrecao() {
+        double valorDesejadoFosforo;
+        System.out.println("Digite o teor de fósforo a atingir:");
+        valorDesejadoFosforo = (Double.parseDouble(scan.nextLine()));
+        return valorDesejadoFosforo;
+    }
+
+    private static double potassioAposCorrecao(Solo solo) {
+        if (solo.getPotassio()> 0.5) {
+            return solo.getPotassio();
+        } else {
+            if (potassioDesejado(solo) < 0.01) {
+                return solo.getPotassio();
+            } else {
+                return solo.getPotassio()+potassioDesejado(solo);
+            }
+        }
+    }
+
+    private static double potassioDesejado (Solo solo) {
+        double valorDesejado;
+        System.out.println("Digite o valor do potassio desejado para a correção de solo:");
+        valorDesejado = (Double.parseDouble(scan.nextLine()));
+        return (solo.getPotassio()*valorDesejado)/(solo.getPotassio()/ctcCmolCalc(solo)*100) - solo.getPotassio();
     }
 
 
